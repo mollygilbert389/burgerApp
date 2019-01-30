8f0658c91de01356b284fb7c2ab0ddc6404abd90
Molly Gilbert <msgilbert@gmail.com>
2019-01-29 18:13:58 -0600

    adding JAWS

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,23 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "burger_db"
-});
+if (process.env.jawsUrl) {
+  connection = mysql.createConnection(process.env.jawsUrl);
+} else {
+  connection = mysql.createConnection({
+    host: "axxb6a0z2kydkco3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user: "vgskl3lryg5yeifg",
+    password: "r70qn3nyvgcc6x9e",
+    database: "as751k1rrdkog19w"
+  })
+}
+
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "password",
+//   database: "burger_db"
+// });
 
 connection.connect(function(err) {
   if (err) {
